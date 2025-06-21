3877243132bea380d32146bdf9b3875dcd0b7f18
muditha <muditha.kumara@centria.fi>
2025-06-21 04:20:47 +0300

    Task 1: Initialize Git repository and project structure Fixes #1

--- main/main.c
+++ main/main.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "freertos_cpu_monitor.h"
 
 void app_main(void)
 {
     printf("ESP-IDF project structure initialized.\n");
+    freertos_cpu_monitor_init();
 }
